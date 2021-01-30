b3c27fa830c79f0587a2cb9d3aa66a99abc52db4
mauricioMatute <matu_t7@hotmail.com>
2021-01-29 19:44:09 -0600

    Update select_var_y_quita_out_y_red_p2.R

--- Prueba_2/Creación_BD/Preprocesamiento_bases/select_var_y_quita_out_y_red_p2.R
+++ Prueba_2/Creación_BD/Preprocesamiento_bases/select_var_y_quita_out_y_red_p2.R
@@ -1,7 +1,4 @@
 install.packages("corrplot")
-setwd("/Users/paola.mateos/Desktop/Tesis_Mau/prueba_2")
-
-getwd()
 
 base <- read.csv("base_train1_prueba_2.csv", header=TRUE)
 summary(base)
@@ -19,18 +16,15 @@ sd(base$especie)
 
 base <- read.csv("base_test1_prueba_2.csv", header=TRUE)
 #base2<-base[,c(1:5,7,9,12)] #selección de variables train
-base2<-base[,c(1:5,7,9)] #selección de variables
+base2<-base[,c(1:5,7,9)] #selección de variables test
 names(base2)
 summary(base2)
-
-#base_scale<-scale(base2, center = TRUE, scale = TRUE)
-#summary(base_scale)
 
 base3<-base2[base2$CpG_ratio<=3,] #remoción de outliers -> Generación de la BD
 sd(base3$CpG_ratio)
 
 #base_scale<-scale(base3, center = c(0,0,0,0,0,0,0,0), scale = c(1,3,2,2,2,1,1,1)) #reducción train-> Modelado
-base_scale<-scale(base3, center = c(0,0,0,0,0,0,0), scale = c(1,3,2,2,2,1,1)) #reducción -> Modelado
+base_scale<-scale(base3, center = c(0,0,0,0,0,0,0), scale = c(1,3,2,2,2,1,1)) #reducción test-> Modelado
 summary(base_scale)
 
 
