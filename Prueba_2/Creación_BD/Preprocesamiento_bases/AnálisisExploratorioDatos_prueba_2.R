0fa5209477b13c0fd3c9db5829d8ce1fa919de09
mauricioMatute <matu_t7@hotmail.com>
2021-01-29 19:34:57 -0600

    Add files via upload

---
+++ Prueba_2/Creación_BD/Preprocesamiento_bases/AnálisisExploratorioDatos_prueba_2.R
@@ -0,0 +1,62 @@
+
+install.packages("corrplot")
+setwd("/Users/paola.mateos/Desktop/Tesis_Mau/prueba_2")
+
+
+
+
+getwd()
+#
+x <- read.csv("base_train1_prueba_2.csv", header=TRUE)
+
+head(x)
+summary(x)
+base2<-x[,c(1:9,12)]
+
+table(base2$especie)
+
+library(corrplot)
+corrplot.mixed(cor(base2[,1:9]), order="hclust", tl.col="black")
+base_bac<- base2[base2$especie == 0,]
+base_hum<- base2[base2$especie == 1,]
+
+corrplot.mixed(cor(base_bac[,1:9]), order="hclust", tl.col="black")
+corrplot.mixed(cor(base_hum[,1:9]), order="hclust", tl.col="black")
+
+
+dat.pca1 <- prcomp(base2[,c(1:9)], center = TRUE,scale. = TRUE)
+
+
+summary(dat.pca1)
+summary(base2)
+sd(base2$GC_content)
+sd(base2$CpG_ratio)
+sd(base2$dws)
+sd(base2$dry)
+sd(base2$dmk)
+sd(base2$LTP)
+sd(base2$HTP)
+sd(base2$VTP)
+sd(base2$ITP)
+
+
+dat.pca1$rotation
+matri<-dat.pca1$x
+
+install.packages("factoextra")
+library(ggplot2)
+library(factoextra)
+fviz_eig(dat.pca1)
+
+fviz_pca_var(dat.pca1, axes=c(1,3),
+             col.var = "contrib", # Color by contributions to the PC
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+)
+
+fviz_pca_ind(dat.pca1,
+             col.ind = "cos2", # Color by the quality of representation
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+)
+
---
+++ Prueba_2/Creación_BD/Preprocesamiento_bases/select_var_y_quita_out_y_red_p2.R
@@ -0,0 +1,42 @@
+install.packages("corrplot")
+setwd("/Users/paola.mateos/Desktop/Tesis_Mau/prueba_2")
+
+getwd()
+
+base <- read.csv("base_train1_prueba_2.csv", header=TRUE)
+summary(base)
+
+sd(base$GC_content)
+sd(base$CpG_ratio)
+sd(base$dws)
+sd(base$dry)
+sd(base$dmk)
+sd(base$LTP)
+sd(base$HTP)
+sd(base$ITP)
+sd(base$VTP)
+sd(base$especie)
+
+base <- read.csv("base_test1_prueba_2.csv", header=TRUE)
+#base2<-base[,c(1:5,7,9,12)] #selección de variables train
+base2<-base[,c(1:5,7,9)] #selección de variables
+names(base2)
+summary(base2)
+
+#base_scale<-scale(base2, center = TRUE, scale = TRUE)
+#summary(base_scale)
+
+base3<-base2[base2$CpG_ratio<=3,] #remoción de outliers -> Generación de la BD
+sd(base3$CpG_ratio)
+
+#base_scale<-scale(base3, center = c(0,0,0,0,0,0,0,0), scale = c(1,3,2,2,2,1,1,1)) #reducción train-> Modelado
+base_scale<-scale(base3, center = c(0,0,0,0,0,0,0), scale = c(1,3,2,2,2,1,1)) #reducción -> Modelado
+summary(base_scale)
+
+
+
+base_scale_df<-as.data.frame(base_scale)
+write.csv(base_scale_df, "base_test1_prueba_2_prep.csv",row.names = FALSE)
+
+base_CpG<-base2[base2$CpG_ratio>=3,]
+summary(base_CpG)
